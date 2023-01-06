4dc3be529ec9c64537e064a00f4c954ba2a231ef
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-06 11:39:26 +0300

    bind the recycler view with the user orders.

--- app/src/main/java/com/example/teamenvironmentkenya/CustomerOrderActivity.java
+++ app/src/main/java/com/example/teamenvironmentkenya/CustomerOrderActivity.java
@@ -2,19 +2,46 @@ package com.example.teamenvironmentkenya;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.teamenvironmentkenya.adapters.recyclerViews.OrderRecAdapter;
+import com.example.teamenvironmentkenya.database.DatabaseClient;
+import com.example.teamenvironmentkenya.databinding.ActivityCustomerOrderBinding;
+import com.example.teamenvironmentkenya.models.Order;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.List;
+
 public class CustomerOrderActivity extends AppCompatActivity {
+    private ActivityCustomerOrderBinding bind;
+    private List<Order> customerOrders;
+    private OrderRecAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_order);
+        bind = ActivityCustomerOrderBinding.inflate(getLayoutInflater());
+        setContentView(bind.getRoot());
+
+        customerOrders = DatabaseClient.getInstance(this).OrderDao().getUserOrders(1);
+        if (customerOrders.isEmpty()){
+            bind.empty.setVisibility(View.VISIBLE);
+        }else{
+            adapter = new OrderRecAdapter(customerOrders, this);
+            bind.recView.setAdapter(adapter);
+            bind.recView.setLayoutManager(new LinearLayoutManager(this));
+            bind.recView.setHasFixedSize(true);
+        }
+
+
+
+
+        setNavigation(bind.bottom);
     }
     public void setNavigation(BottomNavigationView bottom){
         bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
