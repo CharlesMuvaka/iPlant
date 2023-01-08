9bade149f36f3c7a8cb16ac32c428c7b8888769f
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-08 23:23:34 +0300

    bind the tree recycler adapter with the recycler view.

--- app/src/main/java/com/example/teamenvironmentkenya/fragments/ShopFragment.java
+++ app/src/main/java/com/example/teamenvironmentkenya/fragments/ShopFragment.java
@@ -3,32 +3,32 @@ package com.example.teamenvironmentkenya.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.teamenvironmentkenya.R;
+import com.example.teamenvironmentkenya.adapters.recyclerViews.TreeRecAdapter;
+import com.example.teamenvironmentkenya.database.DatabaseClient;
 import com.example.teamenvironmentkenya.databinding.FragmentShopBinding;
+import com.example.teamenvironmentkenya.models.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ShopFragment extends Fragment {
     private FragmentShopBinding bind;
-
+    private List<Tree> allTrees;
+    private TreeRecAdapter adapter;
+    private DatabaseClient client;
 
     public ShopFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShopFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ShopFragment newInstance(String param1, String param2) {
         ShopFragment fragment = new ShopFragment();
         Bundle args = new Bundle();
@@ -48,6 +48,17 @@ public class ShopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         bind = FragmentShopBinding.inflate(getLayoutInflater());
+
+        allTrees = new ArrayList<>();
+        client = DatabaseClient.getInstance(getContext());
+
+        allTrees = client.TreeDao().getAllTrees();
+        adapter = new TreeRecAdapter(allTrees, getContext());
+
+        bind.recView.setAdapter(adapter);
+        bind.recView.setLayoutManager(new LinearLayoutManager(getContext()));
+        bind.recView.setHasFixedSize(true);
+
         return bind.getRoot();
     }
 }
