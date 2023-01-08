23ad42aee5602fe7a5f42afc4f2a49089d5c77e9
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-01-08 23:15:49 +0300

    define a layout binding instance and inflated its view.

---
+++ app/src/main/java/com/example/teamenvironmentkenya/fragments/ShopFragment.java
@@ -0,0 +1,53 @@
+package com.example.teamenvironmentkenya.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.teamenvironmentkenya.R;
+import com.example.teamenvironmentkenya.databinding.FragmentShopBinding;
+
+
+public class ShopFragment extends Fragment {
+    private FragmentShopBinding bind;
+
+
+    public ShopFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ShopFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ShopFragment newInstance(String param1, String param2) {
+        ShopFragment fragment = new ShopFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        bind = FragmentShopBinding.inflate(getLayoutInflater());
+        return bind.getRoot();
+    }
+}
