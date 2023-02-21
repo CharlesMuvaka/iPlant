e5eec345baf2038b762a795baaf2dded9df929d2
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2023-02-21 03:57:36 +0300

    feat: create a method to initialize the bottom sheet.

--- app/src/main/java/com/example/teamenvironmentkenya/MainActivity.java
+++ app/src/main/java/com/example/teamenvironmentkenya/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.teamenvironmentkenya;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -10,34 +11,51 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.example.teamenvironmentkenya.adapters.recyclerViews.TreeRecAdapter;
 import com.example.teamenvironmentkenya.adapters.recyclerViews.VendorRecAdapter;
 import com.example.teamenvironmentkenya.database.DatabaseClient;
 import com.example.teamenvironmentkenya.databinding.ActivityMainBinding;
+import com.example.teamenvironmentkenya.databinding.BottomSheetBinding;
 import com.example.teamenvironmentkenya.fragments.ShopFragment;
 import com.example.teamenvironmentkenya.fragments.VendorsFragment;
 import com.example.teamenvironmentkenya.models.Constants;
+import com.example.teamenvironmentkenya.models.Tree;
 import com.example.teamenvironmentkenya.models.Vendor;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityMainBinding bind;
     private SharedPreferences pref;
+    private BottomSheetDialog dialog;
+    private List<Tree> allTrees;
+    private TreeRecAdapter adapter;
+    private DatabaseClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bind = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(bind.getRoot());
+
+        dialog = new BottomSheetDialog(this);
+        createDialog();
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -47,6 +65,13 @@ public class MainActivity extends AppCompatActivity {
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         String name = pref.getString(Constants.USER_Name, null);
 
+        allTrees = new ArrayList<>();
+        client = DatabaseClient.getInstance(this);
+
+        allTrees = client.TreeDao().getAllTrees();
+        adapter = new TreeRecAdapter(allTrees, this);
+
+        bind.image2.setOnClickListener(this);
         bind.search1.setQueryHint("Search vendor");
         bind.vendors.setText("Welcome " + name + "!!");
 
@@ -77,6 +102,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void createDialog() {
+        BottomSheetBehavior behavior;
+        View view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet, null, false);
+        BottomSheetBinding binding = BottomSheetBinding.inflate(getLayoutInflater(), null, false);
+        binding.recView.setAdapter(adapter);
+        binding.recView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recView.setHasFixedSize(true);
+
+        dialog.setContentView(view);
+
+        behavior = BottomSheetBehavior.from((View) view.getParent());
+        behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+
+        CoordinatorLayout layout = view.findViewById(R.id.coordinate);
+        assert layout != null;
+        layout.setMinimumHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+    }
+
     public void replaceFragment(Fragment fragment){
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
@@ -91,5 +135,12 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(bind.frameLayout.getId(), new VendorsFragment());
         transaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == bind.image2){
+            dialog.show();
+        }
     }
 }
